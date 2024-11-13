2b55083cf45f168c7ef4c3d32d10bd23429dbf2f
Alexey <top.chel.05@mail.ru>
2024-11-13 20:06:35 +0300

    fixed link

--- src/pages/layout/layout-page.tsx
+++ src/pages/layout/layout-page.tsx
@@ -8,15 +8,17 @@ const Layout: React.FC<Props> = props => {
     return (
         <div className='w-screen flex flex-col items-center'>
             <header className='h-[120px] w-3/4 flex justify-between items-center'>
-                <div className='flex items-center gap-1'>
-                    <div className='h-[64px] w-[64px]'>
-                        <img className='w-full h-full object-cover' src={logo} alt="((" />
+                <Link to='/'>
+                    <div className='flex items-center gap-1'>
+                        <div className='h-[64px] w-[64px]'>
+                            <img className='w-full h-full object-cover' src={logo} alt="((" />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h1 className='text-2xl'>TODO.LIST</h1>
+                            <span>Powerd by Kowlad!</span>
+                        </div>
                     </div>
-                    <div className='flex flex-col'>
-                        <h1 className='text-2xl'>TODO.LIST</h1>
-                        <span>Powerd by Kowlad!</span>
-                    </div>
-                </div>
+                </Link>
                 <nav>
                     <Link to='/todo'>TODO</Link>
                 </nav>
